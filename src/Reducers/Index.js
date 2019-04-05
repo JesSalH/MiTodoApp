992c0e3405249166d875def385cf4fc485f8957f
Jesus <j.salas.hellin@gmail.com>
2019-04-05 10:20:52 +0100

    nuevo cambio prueba

--- src/Reducers/Index.js
+++ src/Reducers/Index.js
@@ -12,7 +12,7 @@ const handleTodosReducer = (oldTodosArray = [], action) => {
   if (action.type === 'ADD_TODO') {
     //   devolvemos un nuevo array con los elementos del antiguo array (separados) y el nuevo toDo
     // Ojo, no usar oldTodosArray.push, se debe devolver un array nuevo 
-    return [...oldTodosArray,action.payload]
+     return [...oldTodosArray,action.payload]
 
   }else if( action.type === 'DELETE_TODO'){
     // devolvemos la lista menos el elemento que nos ha pasado la action.
